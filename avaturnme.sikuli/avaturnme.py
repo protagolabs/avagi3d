013a3d94dd1fd6a3b35f13f9a6627e43e8bffb63
Xing Di (底兴) <xing.di@protagolabs.com>
2024-03-27 12:33:48 -0400

    enhance the code with lackey

--- avaturnme.sikuli/avaturnme.py
+++ avaturnme.sikuli/avaturnme.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import time
+from lackey import *
 # while True:
 
 names = os.listdir("images/")
@@ -18,19 +19,25 @@ while True:
             click(Region(1057,872,89,116))
             click(Region(1761,286,493,696))
             click(Region(1465,347,197,278))
-            click(Region(1107,636,858,41))
-            paste(os.path.join(cwd, "images", name, "image0.png"))
-            click(Region(1956,661,103,46))
+            time.sleep(2)
+            # click(Region(1107,636,858,41))
+            paste(os.path.join(cwd, "images", name, "image2.png"))
+            type(Key.ENTER)
+            # click(Region(1956,661,103,46))
         
             click(Region(1681,345,181,286))
-            click(Region(1107,636,858,41))
-            paste(os.path.join(cwd, "images", name, "image1.png"))
-            click(Region(1956,661,103,46))
+            time.sleep(2)
+            # click(Region(1107,636,858,41))
+            paste(os.path.join(cwd, "images", name, "image0.png"))
+            type(Key.ENTER)
+            # click(Region(1956,661,103,46))
             
             click(Region(1857,329,220,286))
-            click(Region(1107,636,858,41))
-            paste(os.path.join(cwd, "images", name, "image2.png"))
-            click(Region(1956,661,103,46))
+            time.sleep(2)
+            # click(Region(1107,636,858,41))
+            paste(os.path.join(cwd, "images", name, "image1.png"))
+            type(Key.ENTER)
+            # click(Region(1956,661,103,46))
         
             
          
@@ -38,19 +45,21 @@ while True:
         
             
         
-            click(Region(1553,836,292,57))
+            click(find(r"avaturnme.sikuli\male_icon.PNG"))
             click(Region(1626,1260,281,107))
-            sleep(45)
+            time.sleep(45)
         
             
-            click(Region(1840,1304,80,81))
-            click(Region(1394,556,307,174))
+            click(find(r"avaturnme.sikuli\download_icon.png"))
+            click(find(r"avaturnme.sikuli\avatar_icon.png"))
+
+            time.sleep(2)
+            paste(os.path.join(cwd, "charactors", name,  "model.glb"))
+            type(Key.ENTER)       
+            time.sleep(10)    
+            click(find(r"avaturnme.sikuli\x_icon.png"))
+            click(find(r"avaturnme.sikuli\back_icon.png"))
         
-            
-            sleep(10)    
-            click(Region(2099,513,74,100))
-            click(Region(991,109,62,103))
-        
-            shutil.move(r"C:\Users\detio\Downloads\model.glb", os.path.join(cwd, "charactors", name,  "model.glb"))
+            # shutil.move(r"C:\Users\detio\Downloads\model.glb", os.path.join(cwd, "charactors", name,  "model.glb"))
 
    # r.click()
